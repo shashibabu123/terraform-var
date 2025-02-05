b334f090ac1528a437b140a11daa34a66047c666
shashibabu123 <goddream480@gmail.com>
2025-02-05 08:20:37 +0530

    Update Key-pair.tf

--- Key-pair.tf
+++ Key-pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "dove-key" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAStNGipjrLD3YlPNcpPF/fwySc6kpnFmiq8rLamxn3k Shashi Babu@LAPTOP-VQQRIORN"
+  public_key = "<generate your ssh-public-key and paste it>"
 }
